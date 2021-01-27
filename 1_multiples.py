a69706740c0b58ddea3cdb0ff1dadc4eaffcfeba
anthonylschultz <anthonyschultz@gmail.com>
2021-01-26 19:18:25 -0600

    solutions to 1 and 2

---
+++ 1_multiples.py
@@ -0,0 +1,9 @@
+def multiples():
+    ans = 0
+    for i in range(1, 1001):
+        if i % 3 == 0 or i % 5 == 0:
+            ans += i
+
+    return ans
+
+print(multiples())
---
+++ 2_even_fibonacci.py
@@ -0,0 +1,12 @@
+def even_fib():
+    accum = []
+
+    for i in range(1, 3):
+        accum.append(i)
+
+    while sum(accum) < 4000000:
+        accum.append(accum[-1] + accum[-2])
+
+    return accum
+
+print(even_fib())
