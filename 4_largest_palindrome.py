362d755c501d7d1ca379226dfcd66ae0e7254b85
anthonylschultz <anthonyschultz@gmail.com>
2021-01-28 10:16:15 -0600

    4 - largest palindrome

---
+++ 4_largest_palindrome.py
@@ -0,0 +1,18 @@
+def pal():
+    products = []
+    for val_a in range(100, 1000):
+        for val_b in range(100, 1000):
+            products.append(val_a * val_b)
+
+    string_prod = [str(prod) for prod in products]
+    
+    pals = []
+
+    for prod in string_prod:
+        if prod[0] == prod[-1] and prod[1] == prod[-2] and prod[2] == prod[-3]:
+            pals.append(int(prod))
+    return max(pals)
+
+print(pal())
+
+
