ce5b433f236e9ee9b8750ed9b0ec0b860c150f0f
anthonylschultz <anthonyschultz@gmail.com>
2021-02-07 16:07:21 -0600

    began 11, 12

---
+++ 12_high_divis_tri_num.py
@@ -0,0 +1,16 @@
+def high_div():
+    '''
+    1a) find triangle numbers between 1, 100
+    1b) find triangle numbers between 1, 1000
+    2) count factors of each triangle number between 1, 1000
+    3) if factors == 500: grab triangle number
+    '''
+
+    '''
+    1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+    '''
+
+    for i in range(1, 100):
+        i + 
+
+print(high_div())
