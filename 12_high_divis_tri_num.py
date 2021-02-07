3f7ed7a2a284b898d1bdac80e46b439434d8075d
anthonylschultz <anthonyschultz@gmail.com>
2021-02-07 16:48:44 -0600

    completed 12

--- 12_high_divis_tri_num.py
+++ 12_high_divis_tri_num.py
@@ -1,16 +1,28 @@
+import numpy as np 
 def high_div():
-    '''
-    1a) find triangle numbers between 1, 100
-    1b) find triangle numbers between 1, 1000
-    2) count factors of each triangle number between 1, 1000
-    3) if factors == 500: grab triangle number
-    '''
 
-    '''
-    1, 3, 6, 10, 15, 21, 28, 36, 45, 55
-    '''
+# 1) find triangle numbers between add_by 1, 100
 
-    for i in range(1, 100):
-        i + 
+    add_by = np.arange(0, 500, 1)
+    
+    d_v = dict()
+    accum = 0
+
+    for val in add_by:
+        accum += val
+        d_v[accum] = 0
+    
+# 2) calculate num of factors of each val, pack into lst
+
+    for k in d_v:
+        for i in range(1, k+1):
+            if k % i == 0:
+                d_v[k] += 1
+
+# 3) return first triangle number with more than 100 divisors
+
+    for k, v in d_v.items():
+        if v >= 100:
+            return k
 
 print(high_div())
