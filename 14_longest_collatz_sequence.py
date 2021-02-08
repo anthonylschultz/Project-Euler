6e1e920b1a1769c519f4fb44e5063c10c28faf1f
anthonylschultz <anthonyschultz@gmail.com>
2021-02-08 10:31:09 -0600

    14 progress

--- 14_longest_collatz_sequence.py
+++ 14_longest_collatz_sequence.py
@@ -1,22 +1,29 @@
-def col_seq():
-    '''
-    1) generate col sequence until 1 - print col sequence
-    2) calculate length of sequence
-    3) generate col sequence for various numbers, calculate length of sequences
-    '''
+def col_seq(start, stop):
 
-    
+    d_col = dict()
+    def len_seq(start):
+        val = start
+        val_lst = []
+        while val != 1:
+            if val % 2 == 0:
+                val /= 2
+                val_lst.append(int(val))
+            else:
+                val = ((3*val) + 1)
+                val_lst.append(int(val))
+        d_col[start] = len(val_lst)
+
+        return d_col
+
+    for start in range(start, stop):
+        len_seq(start)
+        
+    print(len_seq(start))
+
+print(col_seq(13, 200))
 
 
 
-
-
-
-
-
-
-
-
-
-
-print(col_seq())
+'''
+dictionary[val] = len(sequence)
+'''
