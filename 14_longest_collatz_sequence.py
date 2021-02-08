d91c0022420eb6540db4e1382b03a84acb49a546
anthonylschultz <anthonyschultz@gmail.com>
2021-02-08 09:48:37 -0600

    14 in progress

---
+++ 14_longest_collatz_sequence.py
@@ -0,0 +1,22 @@
+def col_seq():
+    '''
+    1) generate col sequence until 1 - print col sequence
+    2) calculate length of sequence
+    3) generate col sequence for various numbers, calculate length of sequences
+    '''
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(col_seq())
