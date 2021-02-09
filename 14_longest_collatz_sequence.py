0444c3ebcd889ecd63d7d67361e3192f720e538f
anthonylschultz <anthonyschultz@gmail.com>
2021-02-09 09:53:17 -0600

    added doc string to 14, began 15

--- 14_longest_collatz_sequence.py
+++ 14_longest_collatz_sequence.py
@@ -1,4 +1,20 @@
 def col_seq(start, stop):
+    '''
+    The following iterative sequence is defined for the set of positive integers:
+
+    n → n/2 (n is even)
+    n → 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+
+    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+    It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), 
+    it is thought that all starting numbers finish at 1.
+
+    Which starting number, under one million, produces the longest chain?
+
+    NOTE: Once the chain starts the terms are allowed to go above one million.
+    '''
 
     d_col = dict()
     def len_seq(start):
@@ -24,14 +40,14 @@ def col_seq(start, stop):
 
 
 # Print Output  
-    # for k, v in d_col.items():
-    #     print(f'Starting Value: {k}, Chain Length: {v}')
+#     for k, v in d_col.items():
+#         print(f'Starting Value: {k}, Chain Length: {v}')
 
 # Active Collatz Sequence
     print(len_seq(start))
 
 # Activate Entire Function
-print(col_seq(13, 1000001))
+print(col_seq(13, 1000))
 
 
 
---
+++ 15_lattice_paths.py
@@ -0,0 +1,26 @@
+def lat_paths():
+    '''
+    Premise:
+    Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
+    there are exactly 6 routes to the bottom right corner.
+
+    How many such routes are there through a 20x20 grid?
+    '''
+
+'''
+2x2
+4 squares
+6 paths
+
+3x3
+9 squares
+
+20x20
+400 squares
+? paths
+'''
+
+
+
+
+print(lat_paths())
